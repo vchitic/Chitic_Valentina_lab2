fb0fe5658120aad524cb55cfcb7a89c7138f070b
Valentina Chitic <valentina.chriss@hotmail.com>
2020-10-27 11:40:14 +0200

    laborator 4

---
+++ StopCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace Chitic_Valentina_lab2.CustomCommands
+{
+    class StopCommand
+    {
+        private static RoutedUICommand launch_command;
+
+        static StopCommand()
+        {
+            InputGestureCollection myInputGestures = new InputGestureCollection();
+            myInputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            launch_command = new RoutedUICommand("Launch", "Launch", typeof(StopCommand), myInputGestures);
+        }
+
+        public static RoutedUICommand Launch
+        {
+            get
+            {
+                return launch_command;
+            }
+        }
+    }
+}
